f8432de0ab5c5bdc733955f0edfa9bbd9cc12be8
JacobRajah <jacob.rajah@mail.utoronto.ca>
2020-06-05 12:42:28 -0400

    change to port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/songfound', (req, res) => {
     res.json(song);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
 
