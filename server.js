d33f31d07735ef71f2e0a958a96c206af420c4ee
JacobRajah <jacob.rajah@mail.utoronto.ca>
2020-06-05 11:52:32 -0400

    Pushing Horoku files

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/songfound', (req, res) => {
     res.json(song);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
 
