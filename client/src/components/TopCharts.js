8fc252b49255f8cf6a3f0e9ff3cde0035275bb5b
JacobRajah <jacob.rajah@mail.utoronto.ca>
2020-10-14 23:55:46 -0400

    Changes to TopCharts page

--- client/src/components/TopCharts.js
+++ client/src/components/TopCharts.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import TopBar from './topbar';
 import './TopCharts.css'
+import './home.css'
 
 class TopCharts extends Component {
     render (){
         return (
-            <div className="TC">
-                <h1>TOP CHARTS</h1>
+            <div className="App">
+                <div className="App-beta">
+                    <TopBar></TopBar>
+                </div>
+                <div className="App-header">
+                    <div className="TC">
+                        
+                    </div>
+                </div>
             </div>
         )};
 }
